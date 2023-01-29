83ad2bcbc0857b3f38a6b337bcd74ea6a001a030
anupshaw <shawanup871@gmail.com>
2023-01-29 13:12:23 +0530

    updated the project

--- src/Emailbody.js
+++ src/Emailbody.js
@@ -2,6 +2,7 @@ import React from "react";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import LabelOutlinedIcon from "@mui/icons-material/LabelOutlined";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import "./CSS/emaillist.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -9,12 +10,12 @@ import { mailAction } from "./features/mailSlice";
 import { db } from "./firebase";
 
 function Emailbody(props) {
-  const { data,id,senderName, subject,senderEmail, message,time } = props;
-  console.log('email',senderEmail)
+  const { data, id, senderName, subject, senderEmail, message, time } = props;
+  console.log("email", senderEmail);
   const dispatch = useDispatch();
   const history = useHistory();
-  const currentUser=useSelector(state=>state.user.value.email)
-  const mailType=useSelector(state=>state.mail.mailType)
+  const currentUser = useSelector((state) => state.user.value.email);
+  const mailType = useSelector((state) => state.mail.mailType);
 
   const openMessage = () => {
     dispatch(
@@ -25,38 +26,56 @@ function Emailbody(props) {
         senderEmail,
         message,
         time,
-        id
+        id,
       })
     );
-  
 
-    db.collection('ReceivedMails').doc(currentUser).collection('mail').doc(id).update({
-      ...data,
-      read:true
-    })
+    db.collection("ReceivedMails")
+      .doc(currentUser)
+      .collection("mail")
+      .doc(id)
+      .update({
+        ...data,
+        read: true,
+      });
     history.push("/mail");
-
   };
 
-//   const updateRead=()=>{
-// db.collection('ReceivedMails').doc(currentUser).collection('mail').doc(id).update({
-//   ...data,
-//   read:true
-// })
-//   }
+  //   const updateRead=()=>{
+  // db.collection('ReceivedMails').doc(currentUser).collection('mail').doc(id).update({
+  //   ...data,
+  //   read:true
+  // })
+  //   }
   return (
-    <div className={`emailbody ${data.read===true && 'emailbody--unread'}`} onClick={(e) => openMessage()}>
-      <div className={`emailbody__left ${data.read===true && 'emailbody__left--unread'}`}>
-        <CheckBoxOutlineBlankIcon />
-        <StarBorderIcon />
-        <LabelOutlinedIcon />
-        {
-          mailType==='sent'? <h4>To:{data.recipents}</h4>:<h4>{senderName}</h4>
-        }
-       
+    <div
+      className={`emailbody ${data.read === true && "emailbody--unread"}`}
+      onClick={(e) => openMessage()}
+    >
+      <div
+        className={`emailbody__left ${
+          data.read === true && "emailbody__left--read"
+        }`}
+      >
+        <div className="emailbody__leftLabel">
+          <CheckBoxOutlineBlankIcon />
+          <StarBorderIcon />
+          <LabelOutlinedIcon />
+          {data.read === false && <FiberManualRecordIcon className="msgRead" />}
+        </div>
+
+        {mailType === "sent" ? (
+          <h4>To:{data.recipents}</h4>
+        ) : (
+          <h4>{senderName}</h4>
+        )}
       </div>
       <div className="emailbody__middle">
-        <div className={`emailbody__middle__msg ${data.read===true && 'emailbody__middle__msg--unread'}`}>
+        <div
+          className={`emailbody__middle__msg ${
+            data.read === true && "emailbody__middle__msg--read"
+          }`}
+        >
           <p>
             <b>{subject}</b> {message}
           </p>
